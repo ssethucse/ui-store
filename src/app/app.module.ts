396ddb114a95b221b95703719c4d48a163177036
sethu <ssethucse@gmail.com>
2023-01-28 22:23:57 +0530

    shopping cart crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,11 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 
 const routes: Routes = [
+{path: 'cart-details', component: CartDetailsComponent},
 {path: 'product/getInfo/:id', component: ProductDetailsComponent},
 {path: 'product/getProducts/search/:keyword', component: ProductListComponent},
 {path: 'product/getProducts/:id', component: ProductListComponent},
@@ -29,7 +31,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
