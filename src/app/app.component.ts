67a82d956fbd40a024bce02fbdd365f6fe49e4a5
sethu <ssethucse@gmail.com>
2023-02-27 20:49:10 +0530

    categories added in bottom removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ui-store';
 
-  contacts(){
-    alert("test");
-  }
+   productCategories: ProductCategory[]=[];
+    constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.listProductCategories();
+    }
+
+   listProductCategories(){
+    this.productService.getProductCategories().subscribe(
+    data=> {
+    this.productCategories = data;
+    }
+    );
+    }
 }
